0a120a1551476e1551903968c424a3604c67f0a9
mars <a2261214089@qq.com>
2023-11-01 19:43:33 +0800

    hong ding yi

--- hong_ding_yi.c
+++ hong_ding_yi.c
@@ -1,33 +1,11 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef int (*Predicate)(int);
-
-int *filter(int *array, int length, Predicate predicate, int *resultLength) {
-    int *result = (int *)malloc(length * sizeof(int));
-    int count = 0;
-    for (int i = 0; i < length; i++) {
-        if (predicate(array[i])) {
-            result[count++] = array[i];
-        }
-    }
-    *resultLength = count;
-    return result;
-}
-
-int isPositive(int num) {
-    return num > 0;
-}
-
+#define CAL(a) a * a * a
+#define MAGIC_CAL(a, b) CAL(a) + CAL(b)
 int main(void) {
-    int array[] = {-3, -2, -1, 0, 1, 2, 3, 4, 5, 6};
-    int length = sizeof(array) / sizeof(array[0]);
-    int resultLength;
-    int *filteredNumbers = filter(array, length, isPositive, &resultLength);
-    for (int i = 0; i < resultLength; i++) {
-        printf("%d ", filteredNumbers[i]);
-    }
-    printf("\n");
-    free(filteredNumbers);
-    return 0;
+    int nums = 1;
+    if(16 / CAL(2) == 2) {
+        printf("I'm TWO(ﾉ>ω<)ﾉ\n");
+    } else {
+        int nums = MAGIC_CAL(++nums, 2);//未定义行为
+    }//因为块作用域，所以在其中重定义的nums出来后被销毁了，nums的值等于1
+    printf("%d\n", nums);
 }
